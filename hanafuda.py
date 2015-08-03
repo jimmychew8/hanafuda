823e0dda370f435728a6f0f7ba034ef223286b95
James Chu <jameschu809@gmail.com>
2015-08-03 00:45:58 -0400

    Fixed recursion and formatting

--- hanafuda.py
+++ hanafuda.py
@@ -1,22 +1,3 @@
-score_tally = []
-get_month = { 
-
-	0 : "January", 
-	1 : "February",
-	2 : "March",
-	3 : "April",
-	4 : "May", 
-	5 : "June",
-	6 : "July",
-	7 : "August",
-	8 : "September",
-	9 : "October",
-	10 : "November",
-	11 : "December"
-
-	}
-
-
 def get_player_score(player_number, month):
 	score = None 
 	while not isinstance(score, int): 
@@ -27,20 +8,16 @@ def get_player_score(player_number, month):
 	return score 
 
 
-def do_scores_add_up(player_one, player_two, player_three): 
-	if player_one + player_two + player_three == 0:
-		return True 
-
 def scores_for_the_month_of(month):
 	player_1 = get_player_score(1, month) 
 	player_2 = get_player_score(2, month) 
 	player_3 = get_player_score(3, month)
-	if do_scores_add_up(player_1, player_2, player_3):
+	if  player_1 + player_2 + player_3 == 0:
 		print player_1, player_2, player_3 
 		return player_1, player_2, player_3 
 	else: 
 		print "-----SCORES DON'T ADD UP-----"
-		scores_for_the_month_of(month)
+		return scores_for_the_month_of(month)
 
 
 def main(): 
@@ -48,13 +25,28 @@ def main():
 		print "-----%r-----" % get_month[month]
 		score_tally.insert(month, scores_for_the_month_of(month)) 
 		print score_tally
-		
-		"""
 		player_1 = sum(score_tally[i][0] for i in range(month+1))
 		player_2 = sum(score_tally[i][1] for i in range(month+1))
 		player_3 = sum(score_tally[i][2] for i in range(month+1))
 		print "Total: Player 1: %r, Player 2: %r, Player 3: %r" % (player_1, player_2, player_3)
-		"""
 
-main()
+
+if __name__ == '__main__': 
+
+	score_tally = []
+	get_month = { 0 : "January", 
+				  1 : "February",
+				  2 : "March",
+				  3 : "April",
+				  4 : "May", 
+				  5 : "June",
+				  6 : "July",
+				  7 : "August",
+				  8 : "September",
+				  9 : "October",
+				 10 : "November",
+				 11 : "December"}
+
+	main()
+
 
