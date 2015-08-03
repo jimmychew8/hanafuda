6f2618e9d8a604d2d544171d592c1f0174c3571d
James Chu <jameschu809@gmail.com>
2015-08-03 00:58:21 -0400

    Format idiomatically

--- hanafuda.py
+++ hanafuda.py
@@ -28,17 +28,17 @@ def main():
 if __name__ == '__main__': 
 	score_tally = []
 	get_month = { 0 : "January", 
-				  1 : "February",
-				  2 : "March",
-				  3 : "April",
-				  4 : "May", 
-				  5 : "June",
-				  6 : "July",
-				  7 : "August",
-				  8 : "September",
-				  9 : "October",
-				 10 : "November",
-				 11 : "December" }
+		      1 : "February",
+		      2 : "March",
+		      3 : "April",
+		      4 : "May", 
+		      5 : "June",
+	 	      6 : "July",
+		      7 : "August",
+		      8 : "September",
+		      9 : "October",
+		     10 : "November",
+		     11 : "December" }
 	main()
 
 
